3cf8b9ac0513f3dbd35759534a8387fa915b2034
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-04 00:50:13 +0300

    Less3

---
+++ Les2_ex_1/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 555;
+int b1 = 7777;
+int c1 = 12;
+int a2 = 14;
+int b2 = 777;
+int c2 = 9;
+int a3 = 32;
+int b3 = 7777777;
+int c3 = 331;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+System.Console.WriteLine(max);
---
+++ z1/Program.cs
@@ -0,0 +1,110 @@
+﻿void Method1()
+{
+    System.Console.WriteLine("Автор:   ");
+}
+//Method1();
+
+
+//vid2
+void Method2(string msg)
+{
+    System.Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения: ");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Text: ", 4);
+//Method21(count:4,msg: "New text");
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+//System.Console.WriteLine(year);
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "kek01");
+//System.Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        //System.Console.WriteLine($"{i}*{j}={i * j}");
+    }
+    //System.Console.WriteLine();
+}
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – "
++ "что, ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля. Вы так"
++ "красноречивы. Вы дадите мне чаю?";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + ($"{newValue}");
+        else result = result + text[i];
+    }
+    return result;
+}
+string NewText = Replace(text, ' ', '|');
+//System.Console.WriteLine();
+NewText = Replace(NewText, 'к', 'К');
+NewText = Replace(NewText, 'С', 'с');
+// System.Console.WriteLine(NewText);
+
+int[] arr = { 1, 5, 4, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.LongLength; i++)
+    {
+        int minPosition = 1;
+        for (int j = i + 1; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
